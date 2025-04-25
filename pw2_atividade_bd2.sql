b73641128bf82ba5bf2eecb2692b074648ce0bd1
Maria Manuela <manuela92araujo@gmail.com>
2025-04-24 21:41:07 -0300

    Update pw2_atividade_bd2.sql
    Código atualizado corrigindo alguns erros

--- pw2_atividade_bd2.sql
+++ pw2_atividade_bd2.sql
@@ -22,5 +22,18 @@ CREATE TABLE login(
 
 INSERT INTO usuario (nome, data_nascimento, cpf, email, senha) VALUES ("Fernanda Silva", "1997-01-02", "123.456.789-00", "fernanda.silva@email.com", "123456abc");
 INSERT INTO login (email, hash_senha) VALUES ("fernanda.silva@email.com", "123456abc");
+INSERT INTO usuario (nome, data_nascimento, cpf, email, senha) VALUES ("Ana Luiza", "1996-05-02", "222.333.111-00", "analuiza1@email.com", "abcd1234");
+INSERT INTO login (email, hash_senha) VALUES ("analuiza1@email.com", "abcd1234");
 
-SELECT*FROM usuario;
+UPDATE login
+SET id_usuario = 1
+WHERE id_login= 1;
+UPDATE login
+SET id_usuario = 2
+WHERE id_login= 2;
+UPDATE usuario
+SET data_nascimento = "1997-01-02"
+WHERE id_usuario= 1;
+
+SELECT*FROM login
+INNER JOIN usuario ON login.id_usuario=usuario.id_usuario;
